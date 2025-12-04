440f0bd1dfd20c3fd4a0ab4efe4676ea1f7b1d84
Raif Shareef <raif.sh94@gmail.com>
2025-12-04 17:23:57 +0800

    building tree recursively

--- app.js
+++ app.js
@@ -8,14 +8,36 @@ class Node {
 
 class Tree {
     constructor(arr) {
-        this.arr = this.buildTree(arr);
+        this.root = this.buildTree(arr);
     }
 
     // buildTree(array) function that takes an array 
     // and turns it into a balanced binary tree full of 
     // `Nodes` objects in appropriate place
     buildTree(array) {
+        // remove duplicates and sort the array
+        const uniqueSortedArr = [...new Set(array)].sort((a, b) => a - b);
+
+        // build tree using array, start and end index
+        return this.buildTreeRecursively(uniqueSortedArr, 0, uniqueSortedArr.length-1);
+    }
+
+    // Recursive function to build tree
+    buildTreeRecursively(arr, start, end) {
+        // define base function, checking if index of start is greater than end index
+        if (start > end) {
+            return null;
+        }
+
+        // find the middle element and make it the root
+        const mid = Math.floor((start + end) / 2);
+        const node = new Node(arr[mid]);
+
+        // recusively construct the left and right subtrees
+        node.left = this.buildTreeRecursively(arr, start, mid - 1);
+        node.right = this.buildTreeRecursively(arr, mid + 1, end);
         
+        return node;
     }
 
     // insert(value) function
@@ -34,3 +56,11 @@ const prettyPrint = (node, prefix = '', isLeft = true) => {
     prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
   }
 };
+
+
+const testArr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+
+const test_tree = new Tree(testArr);
+
+// console.log(test_tree.root)
+prettyPrint(test_tree.root)
